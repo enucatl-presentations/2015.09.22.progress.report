52a4c1655661942fb6490995d0ec5e8b6d360575
Matteo Abis <gmatteo..abis@gmail.com>
2015-09-17 15:29:30 +0200

    Automated commit at 2015-09-17 13:29:30 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -4513,7 +4513,8 @@ Reveal.initialize({
     controls: true,
     progress: true,
     history: true,
-    center: true,
+    center: false,
+    slideNumber: 'c/t',
     transition: 'none', // none/fade/slide/convex/concave/zoom
     // Optional reveal.js plugins
     dependencies: [
@@ -4522,7 +4523,8 @@ Reveal.initialize({
         { src: 'vendor/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
         { src: 'vendor/reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
         { src: 'vendor/reveal.js/plugin/zoom-js/zoom.js', async: true },
-        { src: 'vendor/reveal.js/plugin/notes/notes.js', async: true }
+        { src: 'vendor/reveal.js/plugin/notes/notes.js', async: true },
+        { src: 'vendor/reveal.js/plugin/math/math.js', async: true }
     ]
 });
 var link = document.createElement( 'link' );
